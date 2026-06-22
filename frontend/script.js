4503cff30b3114c24b7704757f75aa3e830e66f3
Natasha M <nmulumbatech@gmail.com>
2026-06-22 21:47:22 +0200

    created script.js and added chatbot btn open and close functionality

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+// CHATBOT Functionality //
+
+const toggleBtn = document.querySelector("#chat-toggle-btn")
+const chatWindow= document.querySelector("#chat-window")
+const closeBtn = document.querySelector("#close-button")
+
+toggleBtn.addEventListener("click",function(){
+    chatWindow.classList.add("open");
+});
+
+closeBtn.addEventListener("click",function(){
+    chatWindow.classList.add("open");
+});
