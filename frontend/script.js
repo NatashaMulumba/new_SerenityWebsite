12e3fc9a51057adf890e860786dea2bc933b5ff7
Natasha M <nmulumbatech@gmail.com>
2026-06-25 14:34:54 +0200

    updated script.js, added event listener for hamburger menu, and each nav link

--- frontend/script.js
+++ frontend/script.js
@@ -3,11 +3,33 @@
 const toggleBtn = document.querySelector("#chat-toggle-btn")
 const chatWindow= document.querySelector("#chat-window")
 const closeBtn = document.querySelector("#close-button")
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".navbar-menu")
 
+// When Book session clicked, open up chatbot window //
 toggleBtn.addEventListener("click",function(){
     chatWindow.classList.add("open");
 });
 
+//When X button on chatheader selected, hide chatbot window//
 closeBtn.addEventListener("click",function(){
     chatWindow.classList.remove("open");
 });
+
+//Open Menu when clicked//
+hamburger.addEventListener("click",function(){
+    navMenu.classList.toggle("menu-open");
+
+    const isOpen = navMenu.classList.contains("menu-open");
+    hamburger.setAttribute("aria-expanded",isOpen);
+});
+
+// find the navigation links and listen for when a link is clicked 
+navMenu.querySelectorAll("a").forEach(function(link){
+    link.addEventListener("click",function() {
+        navMenu.classList.remove("menu-open");
+        hamburger.setAttribute("aria-expanded", false);
+    });
+});
+
+
